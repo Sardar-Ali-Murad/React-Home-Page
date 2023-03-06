e4437305600d8f18b9c0fc1d853bdc8d3028dd61
your_username <sardaralimuradali4@gmail.com>
2023-03-06 11:18:05 +0500

    xzsxs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Navbar,About,Services,Benefit,Join,Team,Vedios,Login,Ideas,FAQ,Footer}  from "./components/index"
+import "./App.css"
 
 const App = () => {
   return (
